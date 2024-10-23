96b3cb7ed73a97a6f405bb37d18061aad1563b78
mengzhuou <a1106983163@gmail.com>
2024-10-23 16:09:27 -0400

    initialize project

---
+++ src/Chatbox.tsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo-removebg-preview.png';
+import { withFuncProps } from "./withFuncProps";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'; // Import the specific icon you need
+import axios from 'axios';
+
+class Chatbox extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      prompt: "",
+      response: "",
+    }
+  }
+
+  localNav = () => {
+    this.props.navigate("/")
+  }
+
+  handleInputChange = (event: { target: { value: any; }; }) => {
+    this.setState({ prompt: event.target.value });
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Form submitted!");
+  }
+
+  handleAISubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5001/generate', { prompt: this.state.prompt });
+      const response = res.data.response;
+      window.alert(res.data.response);
+      this.setState({ response: res.data.response });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  render() {
+    const { prompt, response } = this.state;
+    return (
+      <div className="App">
+        <div className='container'>
+          <h1 className="title">AI Speech Recognizer</h1>
+          <img className="logo_img" src={logo} alt="Logo" />
+          <p className='midLineChatbox'>
+            You can speak or type any questions. How can I help you? 
+          </p>
+          <div className="inputSection">
+            <input
+              className="inputBar"
+              type="text"
+              placeholder="Message Traversal AI ..."
+              value={prompt}
+              onChange={this.handleInputChange}
+            />
+            <button type="submit" className="submitBtn" onClick={this.handleAISubmit}>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </div>
+          {response && <p>{response}</p>}
+        </div>
+
+        <footer>
+          <div className="footer-text">
+            Empower Your Journey @
+          </div>
+          <div className="footer-find-me">
+            <a onClick={this.localNav}>AI Speech Recognizer</a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default withFuncProps(Chatbox);
---
+++ src/Introduction.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo-removebg-preview.png';
+import hotelLogo from './hotel_logo.png';
+import { withFuncProps } from "./withFuncProps";
+
+class Introduction extends React.Component<any,any>{
+    constructor(props:any){
+        super(props);
+    }
+
+    chatBoxNav = () => {
+        this.props.navigate("/Chatbox")
+    }
+
+    localNav = () => {
+        window.location.reload(); // Reload the page
+        window.scrollTo(0, 0); 
+    }
+    render() {
+        return (
+        <div className="App">
+            <div className="container">
+            <h1 className="title">AI Speech Recognizer</h1>
+            <img className="logo_img" src={logo} alt="Logo" />
+            <div className='row'>
+                <div className='col'>
+                <div className='topLine'>
+                    <div className='topLineText'>Personalized</div>
+                    <div className='topLineText'> | </div>
+                    <div className='topLineText'>24/7</div>
+                    <div className='topLineText'> | </div>
+                    <div className='topLineText'>Self-paced</div>
+                </div>
+                    <p className='topText'>Experience hassle-free travel planning with <span className='recommenderName'>AI Speech Recognizer</span></p>
+                    <p className='midLine'>Simplify your search and make your decision-making process a breeze.</p>
+                </div>
+                <div className='col-6'>
+                <img className="hotel_logo_img" src={hotelLogo} alt="hotel Logo" />
+                </div>
+                </div>
+                <button className="btnStyle" onClick={this.chatBoxNav}>LET'S START</button>
+            </div>
+            <footer>
+                <div className="footer-text">
+                    Empower Your Journey @
+                </div>
+                <div className="footer-find-me">
+                    <a onClick={this.localNav}>AI Speech Recognizer</a>
+                </div>
+            </footer>
+        </div>
+        );
+    }
+}
+
+export default withFuncProps(Introduction);
